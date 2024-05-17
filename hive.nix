c4130cd5222bfe864d6b486356cb226829e50d3f
Marc Schlagenhauf <marc@schlagenhauf.net>
2024-05-17 19:39:39 +0200

    Switch Keycloak to unstable

--- hive.nix
+++ hive.nix
@@ -7,7 +7,7 @@ inputs:
     nixpkgs = import inputs.nixpkgs {
       system = "x86_64-linux";
       overlays = [
-        (final: prev: { inherit (import inputs.nixpkgs-unstable { system = prev.system; }) zammad; })
+        (final: prev: { inherit (import inputs.nixpkgs-unstable { system = prev.system; }) zammad keycloak; })
       ];
     };
 
@@ -17,6 +17,8 @@ inputs:
   defaults = ./common;
 
   # hosts
+  automatix = ./hosts/automatix;
+  barometrix = ./hosts/barometrix;
   majestix = ./hosts/majestix;
   miraculix = ./hosts/miraculix;
   obelix = ./hosts/obelix;

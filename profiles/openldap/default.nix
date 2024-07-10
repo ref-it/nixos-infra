1be0f3356dd11a4c39283d49419836af2bbb2674
Marc Schlagenhauf <marc@schlagenhauf.net>
2024-07-10 11:57:03 +0200

    Add web-2-manage to the allow-list for openldap

--- profiles/openldap/default.nix
+++ profiles/openldap/default.nix
@@ -24,6 +24,7 @@ in
         ip6 saddr 2001:638:904:ffbe::191 tcp dport { 389, 636 } accept comment "web-2"
         ip6 saddr 2001:638:904:ffbe::192 tcp dport { 389, 636 } accept comment "web-2"
         ip6 saddr 2001:638:904:ffbe::193 tcp dport { 389, 636 } accept comment "web-2"
+        ip6 saddr 2001:638:904:ffbf::54 tcp dport { 389, 636 } accept comment "web-2-manage"
         ip6 saddr 2001:638:904:ffd0::13 tcp dport { 389, 636 } accept comment "majestix"
         ip6 saddr 2001:638:904:ffd0::15 tcp dport { 389, 636 } accept comment "miraculix"
       '';

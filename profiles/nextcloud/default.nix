02a845608850badd6b058c8ed601948e2d4a9cac
Marc Schlagenhauf <marc@schlagenhauf.net>
2025-03-28 00:31:40 +0100

    Update to Nextcloud 31

--- profiles/nextcloud/default.nix
+++ profiles/nextcloud/default.nix
@@ -66,7 +66,7 @@ in
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud30;
+      package = pkgs.nextcloud31;
       https = true;
       hostName = cfg.fqdn;
       autoUpdateApps.enable = true;

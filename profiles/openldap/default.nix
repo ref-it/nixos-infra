014b9550c0b6f4415866e4959c7fbc62b4ad1120
Marc Schlagenhauf <marc@schlagenhauf.net>
2025-08-05 13:37:55 +0200

    Add internal IP addresses

--- profiles/openldap/default.nix
+++ profiles/openldap/default.nix
@@ -21,6 +21,8 @@ in
     networking.firewall = {
       allowedTCPPorts = [ 80 443 ];
       extraInputRules = ''
+        ip saddr 10.170.20.104 tcp dport { 389, 636 } accept comment "miraculix"
+        ip saddr 10.170.20.106 tcp dport { 389, 636 } accept comment "majestix"
         ip saddr 10.170.20.117 tcp dport { 389, 636 } accept comment "gelantine"
         ip6 saddr 2001:638:904:ffbe::190 tcp dport { 389, 636 } accept comment "web-2"
         ip6 saddr 2001:638:904:ffbe::191 tcp dport { 389, 636 } accept comment "web-2"

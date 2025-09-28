4dfde2eabd7481236a136193a811555e0e8f8117
Marc Schlagenhauf <marc@schlagenhauf.net>
2025-09-28 04:31:02 +0200

    Use zammad from stable, keycloak from unstable

--- hive.nix
+++ hive.nix
@@ -7,7 +7,7 @@ inputs:
     nixpkgs = import inputs.nixpkgs {
       system = "x86_64-linux";
       overlays = [
-        (final: prev: { inherit (import inputs.nixpkgs-unstable { system = prev.system; }) zammad; })
+        (final: prev: { inherit (import inputs.nixpkgs-unstable { system = prev.system; }) keycloak; })
       ];
     };
 

579c51a09c266d2077a5a68b77643bf3e44eca56
Marc Schlagenhauf <marc@schlagenhauf.net>
2025-10-30 01:11:15 +0100

    Pretix backup

--- profiles/pretix/default.nix
+++ profiles/pretix/default.nix
@@ -20,6 +20,14 @@ in
   config = mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = [ 80 443 ];
 
+    sops.secrets = {
+      "borg-passphrase" = {
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
+    };
+
     services.pretix = {
       enable = true;
       gunicorn.extraArgs = [
@@ -51,27 +59,25 @@ in
       environmentFile = "/var/lib/pretix.env";
     };
 
-    /*services.borgbackup.jobs.pretix = {
+    services.postgresqlBackup = {
+      enable = true;
+      startAt = "*-*-* 03:20:00";
+      databases = [ "pretix" ];
+    };
+
+    services.borgbackup.jobs.pretix = {
       user = "root";
       group = "root";
       repo = "ssh://backup:23/./pretix";
-      readWritePaths = [ "/var/lib/pretix/db-backup" ];
-      preHook = ''
-        cd /var/lib/pretix
-
-        rm -f db-backup/*
-
-        ${pkgs.mariadb}/bin/mysqldump ${config.services.pretix.settings.database.name} > db-backup/${config.services.pretix.settings.database.name}.sql
-      '';
-      paths = [ "config/config.php" "data" "db-backup" ];
+      paths = [ "/var/lib/pretix" "/var/backup/postgresql/pretix.sql.gz" ];
       doInit = false;
-      startAt = [ "*-*-* 03:30:00" ];
+      startAt = [ "*-*-* 03:50:00" ];
       encryption.mode = "repokey";
       encryption.passCommand = "cat ${config.sops.secrets."borg-passphrase".path}";
       prune.keep.within = "1y";
       compression = "auto,zstd";
       dateFormat = "+%Y-%m-%d";
       archiveBaseName = "backup";
-    };*/
+    };
   };
 }

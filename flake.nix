e0727df9fa6ba4772c186e2ddf5412f52e1be7c3
Marc Schlagenhauf <marc@schlagenhauf.net>
2025-11-30 23:53:33 +0100

    Update to NixOS 25.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     sops.url = "github:Mic92/sops-nix";

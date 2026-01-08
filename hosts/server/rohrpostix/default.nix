94b82d1350977b36e49e2bec0ef80ab9560967b6
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-08 01:57:27 +0100

    Modify Etherpad proxy settings

--- hosts/server/rohrpostix/default.nix
+++ hosts/server/rohrpostix/default.nix
@@ -87,6 +87,20 @@
               proxyPass = "https://pad2.stura-ilmenau.de/";
               recommendedProxySettings = true;
               proxyWebsockets = true;
+              extraConfig = ''
+                proxy_buffering off;
+                proxy_pass_header Server;
+                proxy_redirect default;
+                client_max_body_size 10m;
+                client_body_buffer_size 128k;
+                proxy_connect_timeout 90;
+                proxy_send_timeout 90;
+                proxy_read_timeout 90;
+                proxy_buffer_size 4k;
+                proxy_buffers 4 32k;
+                proxy_busy_buffers_size 64k;
+                proxy_temp_file_write_size 64k;
+              '';
             };
           };
         };

f04a5429df901998231b134cbe641c698105e0a5
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-03-03 23:23:41 +0100

    Restore majestix

--- hosts/server/majestix/hardware-configuration.nix
+++ hosts/server/majestix/hardware-configuration.nix
@@ -11,23 +11,17 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8f700c1c-7292-4d80-8f97-1a730c2577f5";
+    { device = "/dev/disk/by-uuid/26bd7822-9476-4ee9-b09a-ac2be43fc71d";
       fsType = "btrfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/E689-3277";
+    { device = "/dev/disk/by-uuid/25AD-84D6";
       fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }

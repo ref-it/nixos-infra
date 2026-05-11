8aaab5f829348618af6fe890074aa8bb32af01e2
Maloschnikow <123018032+Maloschnikow@users.noreply.github.com>
2026-05-11 20:46:18 +0200

    update troubadix config

--- hosts/server/troubadix/hardware-configuration.nix
+++ hosts/server/troubadix/hardware-configuration.nix
@@ -14,26 +14,17 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/772dbb6d-1f95-41f1-ae87-3152fcdbaf05";
+    { device = "/dev/disk/by-uuid/452cfc70-8e7e-47c5-ad8e-7d1b0c7b174a";
       fsType = "btrfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/C362-6E74";
+    { device = "/dev/disk/by-uuid/8296-72C4";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/640a6509-e607-4246-9d67-6c35f89dafcc"; }
-    ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }

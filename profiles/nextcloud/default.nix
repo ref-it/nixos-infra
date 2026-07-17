897b7fdd94a54a54c7c4c2deb53223573602c357
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-17 18:36:39 +0200

    Upgrade to Nextcloud 34

--- profiles/nextcloud/default.nix
+++ profiles/nextcloud/default.nix
@@ -56,7 +56,7 @@ in
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud33;
+      package = pkgs.nextcloud34;
       https = true;
       hostName = cfg.fqdn;
       autoUpdateApps.enable = true;
